f73c94c44e89b89810def79fc142cd76aba88396
tombryson <thomas.bryson@outlook.com>
2022-12-06 17:33:24 +1100

    password changes

--- db/migrate/20220724140611_create_users.rb
+++ db/migrate/20220724140611_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.text :email
       t.string :password_digest
+      t.text :password
       t.timestamps
     end
   end
